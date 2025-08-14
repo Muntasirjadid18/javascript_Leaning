13d7cf9136f13dc34d16a4a20d1c69f257219394
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-14 18:30:21 +0600

    push_pop

---
+++ array/push_pop.js
@@ -0,0 +1,21 @@
+//array declaration
+const ages = [12,2,40,20,18];
+const numbers = [];
+console.log(ages);
+//insert a number
+ages.push(10);
+console.log(ages);
+//insert a name
+const friends = ['balam','kalam','salam','rahim','zabbar'];
+friends.push('nilam') 
+console.log(friends);
+//delete a name
+const num1 = friends.pop();
+console.log(friends);
+//delete another name
+const num2 = friends.pop();
+console.log(num1,num2);
+// shift data
+const name = ['shifat','habib','mubin','dihan','rofiq'];
+name.shift();
+console.log(name);
