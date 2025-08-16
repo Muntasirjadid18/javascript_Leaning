9f0aa417563639620dedb18b349af20bdc8310f7
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-16 14:45:51 +0600

    for loop

---
+++ Loop/loop_1.js
@@ -0,0 +1,13 @@
+//node loop_1.js
+//declare array
+const number =[10,20,30,40,50,60,70,80,98]
+console.log(number);
+//declare for loop
+for(const num of number){
+    console.log(num);
+}
+const fruits = ['mango','banana','orange','grapse','tomatoo'];
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log("I want to eat");
+}
