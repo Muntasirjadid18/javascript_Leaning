9a70b68cd1c085bdabd20fa4fa9f0dc47179afc6
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-16 20:24:41 +0600

    drinks

---
+++ Condtiton/Free_Drinks.js
@@ -0,0 +1,14 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+//node Free_Drinks.js
+const burger_Price = 610;
+if(burger_Price>500){
+    console.log("free Coke")
+}
+else{
+    console.log("Coke - 30tk");
+}
