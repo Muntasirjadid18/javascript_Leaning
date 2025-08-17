15666d926d26951304fa2560a2b7638c643bee87
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-17 15:37:37 +0600

    problem

---
+++ Loop/problem_loop.js
@@ -0,0 +1,15 @@
+let sum = 0;
+for(let i =11; i<=20;i++){
+    sum = sum+i;
+    console.log(i);
+}
+console.log("Sum of numbers is 11 to 20 is",sum);
+//decremental for loop
+
+for(let k = 10; k >= 0; k--){
+    console.log(k);
+}
+//LOOP
+for(j=20;j>=11;j--){
+    console.log(j);
+}
