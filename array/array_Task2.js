53abb8d4a9e486ef69c4780ff3deec2a6a03d0a5
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-18 13:32:37 +0600

    push_pop

---
+++ array/array_Task2.js
@@ -0,0 +1,12 @@
+//node array_Task2.js
+// Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+let tourist = ["Cox's Bazar","Sundorban","Kuwakata"];
+tourist.push("kagrachari");//add a new destination
+tourist.push('Kaptai','Bandarban');//add two destination
+tourist.pop();//remove two destination;
+console.log(tourist);
