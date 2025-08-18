657d99c58fdf0589c40b2b01a57de6883307605d
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-18 23:48:44 +0600

    is array

---
+++ array/array_Task4.js
@@ -0,0 +1,12 @@
+// Checking if it's an Array
+// Instructions:
+
+// Create different variables, each containing either an array or a non-array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+const number = [10,20,30,40,50,60,70]
+const string = 'fahim';
+console.log(Array.isArray(number));
+console.log(Array.isArray(string));
