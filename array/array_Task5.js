7573c367ccdea23ffb33da5ef007bfe942a703bf
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-19 12:31:33 +0600

    concat

---
+++ array/array_Task5.js
@@ -0,0 +1,11 @@
+// Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+const num1 = [10,30,50,70,90];
+const num2 = [20,40,60,80,100];
+console.log(num1);
+console.log(num2);
+console.log(num1.concat(num2));
