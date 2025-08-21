9d900e02cce42870f277a59cb06c7cd8623e66e1
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-21 15:30:44 +0600

    string

---
+++ string/string.js
@@ -0,0 +1,19 @@
+const name1 = `Bangladesh`;//backtick     string
+const name2 = "England";//double quotation    string
+const name3 = "Swizerland";//single quotation   string
+const thana = new String("Kotwali");   //object  not basically used
+console.log(typeof thana); 
+const numbers = [54,98,78,21,30,21,51];
+console.log(numbers.length);
+console.log(numbers[2])   // numbers[1,2,3,4,5]
+const  capital = 'Dhaka';
+console.log(capital.length);
+const capital1 = "Dha ka"  // space also count as length;
+// console.log(capital1.length);  
+// console.log(capital[3]); 
+console.log(capital);
+capital[1] = 'q';
+console.log(capital);
+
+//*******sting is immutable(not changebale) 
+// but array is mutable(we can change it)*/
