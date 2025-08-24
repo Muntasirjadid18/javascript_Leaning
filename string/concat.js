b9fc865f97b910ae0ec8a43fab9029ed786a2795
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-24 16:03:02 +0600

    concat

---
+++ string/concat.js
@@ -0,0 +1,5 @@
+const first = 'Abid';
+const last  =' jahid';
+const fullName = first+' '+last;
+console.log(fullName);
+console.log(last.includes('j')); //find the letter;
---
+++ string/slice.js
@@ -0,0 +1,13 @@
+const address = "Andorkilla";
+const part = address.slice(2,5);
+console.log(part);
+const sentence ='I am a good person and hardworking.';
+console.log(sentence.split('')); //show all alphabet;
+console.log(sentence.split(' ')); //show all word;
+console.log(sentence.split('a')); //remove all 'a'
+
+const stringstr = 'Rahim,karim,shofique,jasim,nayeem,sakib';
+const string = stringstr.split(',');
+console.log(string);
+console.log(string.join('|'));
+
