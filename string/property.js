aee266d4e6dbd87f625608505780d02ae544c627
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-26 17:58:33 +0600

    value

---
+++ string/property.js
@@ -0,0 +1,15 @@
+const person ={
+    name : 'sodor uddin',
+    age : 25,
+    profession: 'developer',
+    salary: 25000,
+    married : true,
+    'fav place': ["Sundarban","Cox's bazar","Kuakata"]
+}
+console.log(person.profession);//dot notation and object value access kora
+const income = person.salary;
+console.log(income);
+
+// const keyName = 'profession';
+// console.log(person[keyName])
+
---
+++ string/value.js
@@ -0,0 +1,11 @@
+const person ={
+    name : 'sodor uddin',
+    age : 25,
+    profession: 'developer',
+    salary: 25000,
+    married : true,
+    'fav place': ["Sundarban","Cox's bazar","Kuakata"]
+}
+const propName = 'profession';
+person[propName] ='devops'
+console.log(person)
