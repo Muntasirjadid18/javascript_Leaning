e45b948989e454a5cca6a34f21018afab07995fe
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-08-26 13:49:16 +0600

    object

---
+++ string/object.js
@@ -0,0 +1,23 @@
+//primative datatype
+const array = [10,20,30,40,50]; 
+const age = 20;
+let num = 40;
+
+
+//object - non primative datatype;
+const bottle = {
+    brand : 'Mum',
+    price : 45,
+    color : 'white',
+    isClean : false
+}
+const subject = {
+    name : "Biology",
+    teacher : "Rashed sir",
+    examdate : '30 Feb',
+    chapters: ['First','Second','Third'],
+    exams : {
+       name: "Final Exam",
+       marks: 100
+    }
+}
