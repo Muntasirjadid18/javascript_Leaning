170347f795d25bf665f7e7ce285af7c142dae6ef
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-10-01 16:26:08 +0600

    value

---
+++ string/neested.js
@@ -0,0 +1,20 @@
+const computer = {
+    brand : 'lenovo',
+    price : 35000,
+    processor : 'Intel',
+    hdd : '512gb'
+}
+const keys = Object.keys(computer);
+console.log(keys) //for keys
+const values = Object.values(computer);
+console.log(values)
+const college = {
+    name : 'vnc' ,
+    class : ['11','12'],
+    events : ['Science Fair','Bijoy Dibos','21 Feb'],
+    unique :{
+        color: 'blue'
+    }
+}
+console.log(college.name)
+console.log(college.unique.color)
---
+++ string/values.js
@@ -0,0 +1,8 @@
+const computer = {
+  brand: 'Lenovo',
+  price: 35000,
+  processor: 'intel',
+  hdd: '512gb'
+}
+const values = Object.values(computer);
+console.log(values);
