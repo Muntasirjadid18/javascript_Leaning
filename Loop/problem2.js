9560cee06ede9734af7166a8eefaab1e822bbeb5
Muntasirjadid18 <jadid.muntasir2002@gmail.com>
2025-10-09 01:12:28 +0600

    break

---
+++ Loop/problem2.js
@@ -0,0 +1,11 @@
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+let sum = 0;
+for(let i =1;i<=100;i++){
+sum = sum+i ;
+console.log(sum);
+if(sum>=100){
+    break;
+}
+}
---
+++ Loop/problem3.js
@@ -0,0 +1,9 @@
+
+for(let i = 2;i<=100;i++){
+    let root = Math.sqrt(i);
+    console.log(i);
+    if((Number.isInteger(root))){
+        console.log("First square Number :",i)
+        break;
+    }
+}
