e5cb969a1bb4ae74ed3d5bda9b843b26207a3465
Muntasir Jadid <jadid.muntasir2002@gmail.com>
2025-11-27 13:15:46 +0600

    declare

---
+++ function/declare.js
@@ -0,0 +1,17 @@
+const number = 45;
+//function declare 
+function fanoffkor()
+{
+    console.log("uthe dara");
+    console.log("walk towards the switch");
+    console.log("switch off the fan");
+} 
+//function call 
+fanoffkor();
+function chakhabo()
+{
+    console.log("kitchene jete hbe");
+    console.log("cha banate hbe");
+    console.log("cupeh dalte hbe");
+}
+chakhabo();
