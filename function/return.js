59426e90297499172a3bce6112535e34401ca25e
Muntasir Jadid <jadid.muntasir2002@gmail.com>
2025-11-28 19:24:30 +0600

    return

---
+++ function/return.js
@@ -0,0 +1,13 @@
+function tenTimes(number){
+   const result = number*10;
+   return result;
+}
+function cutHalf(number){
+const half = number/2;
+return half;
+
+}
+tenTimes(10);
+
+const output = tenTimes(10);
+console.log("output",output);
---
+++ string/task3.js
@@ -0,0 +1,11 @@
+let str = "Education".toLowerCase();
+ if(str.includes('a') && 
+    str.includes('e') && 
+    str.includes('i') && 
+    str.includes('o') && 
+    str.includes('u')){
+  console.log("The string contains all the vowels.");
+}
+else{
+console.log("The string doesnot contain all the vowels.");
+}
