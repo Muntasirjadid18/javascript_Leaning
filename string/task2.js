2c72fe46241fb0e999d65119b92faa529a172a3c
Muntasir Jadid <jadid.muntasir2002@gmail.com>
2025-12-10 13:01:29 +0600

    task3

--- string/task2.js
+++ string/task2.js
@@ -1,15 +1,15 @@
 // Count how many times a string has the letter a or A
-let string = 'Apple is a fruits aaaa AAAAA' ;
+let string = "An amazing Apple always attracts attention."
 let count1 = 0;
 let count2 = 0;
-for(i=0;i<string.length;i++){
-if(string[i] ==='a'){
-count1++;
+for(let i=0;i<string.length;i++){
+    if(string[i] === 'a'){
+       count1++;
+    }
+if(string[i] === 'A'){
+       count2++;
+    }
+  
 }
-if(string[i] === "A"){
-    count2++;
-}
-
-}
+console.log(`The letter 'a' appears ${count1}`);
 console.log(`The letter 'A' appears ${count2}`);
- console.log(`The letter 'a' appears ${count1}`);
--- string/task3.js
+++ string/task3.js
@@ -1,11 +1,12 @@
-let str = "Education".toLowerCase();
- if(str.includes('a') && 
-    str.includes('e') && 
-    str.includes('i') && 
-    str.includes('o') && 
-    str.includes('u')){
-  console.log("The string contains all the vowels.");
+// Check whether a string contains all the vowels a, e, i, o, u.
+let string = "Programming is fun";
+if(string.includes('a') &&  //include(a) in the sentence a is present always return true 
+string.includes('e') &&
+string.includes('i') &&
+string.includes('o') &&
+string.includes('u')){
+ console.log("Contains all vowels");
 }
 else{
-console.log("The string doesnot contain all the vowels.");
+  console.log("Does not contain all vowels");
 }
