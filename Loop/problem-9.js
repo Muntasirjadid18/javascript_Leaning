cbc434a97041421d90ae7976d10ae21726fa5540
Muntasir Jadid <jadid.muntasir2002@gmail.com>
2025-12-17 00:19:32 +0600

    p-9

---
+++ Loop/problem-9.js
@@ -0,0 +1,14 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+/*programming hero*/
+
+let multi = 0
+for (let i = 0; i<=10;i++){
+multi = 9 * i ;
+    console.log("9 * " + i + " = " + multi  );
+}
