fc817252ba1db7366ea69b9c1a05e41dcc82c599
Muntasir Jadid <jadid.muntasir2002@gmail.com>
2025-12-17 01:35:55 +0600

    15

---
+++ Loop/probelm14.js
@@ -0,0 +1,15 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+
+/*programming hero*/
+let i =0;
+let mul = 0;
+while(i<=10){
+mul = 5*i;
+i++;
+console.log("5 * "+ i +" = " +mul)
+}
